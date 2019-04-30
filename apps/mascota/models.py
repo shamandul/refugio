a8ae557200eb83205fa9a38023ae412ba7f8f109
Jesús de Serdio <shamandul@gmail.com>
2019-04-30 22:56:13 +0200

    Creado modelos y migraciones de mascota y adopciones

--- apps/mascota/models.py
+++ apps/mascota/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Mascota(models.Model):
+    folio = models.CharField(primary_key=True, max_length=10)
+    nombre = models.CharField(max_length=50)
+    sexo = models.CharField(max_length=10)
+    edad_aproximada = models.IntegerField()
+    fecha_rescate = models.DateField()
