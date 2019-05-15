f7e5d0427ca4e93b1505682122ee95b4bbc5028e
Jesús de Serdio <shamandul@gmail.com>
2019-05-15 21:29:07 +0200

    archivo inicio para produccion

---
+++ index.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Add the project to the python path
+import os, sys
+
+sys.path.append('/home/webweave/ refugio.webweaver.es/refugio/')
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
+
+# Prevent mod_wsgi from erroring on stdout access (WSGIRestrictStdout Off)
+sys.stdout = sys.stderr
+
+os.environ['PYTHON_EGG_CACHE'] = '/home/webweave/tmp/trac-eggs/'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'refugio.settings'
+
