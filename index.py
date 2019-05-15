3c453860eed004aa422f57eed310ab78c329bc83
Jesús de Serdio <shamandul@gmail.com>
2019-05-15 22:09:17 +0200

    correcion del index para produccion

--- index.py
+++ index.py
@@ -24,31 +24,4 @@ except:
   traceback.print_exc(file=f)
 f.close()
 
-# Debug middleware
-if settings.DEBUG:
-    print >> sys.stderr, "Using Paste error middleware"
-    from paste.exceptions.errormiddleware import ErrorMiddleware
 
-    application = ErrorMiddleware(application, debug=True, show_exceptions_in_wsgi_errors=True)
-
-# Create session directory if not present
-if settings.SESSION_FILE_PATH:
-    try:
-        os.makedirs(settings.SESSION_FILE_PATH)
-    except OSError:
-        pass
-
-
-def application(environ, start_response):
-    import sys
-    output = 'Python version: ' + str(sys.version) + '<br/>'
-
-    import django
-    output += 'Django version: ' + str(django.VERSION) + '<br/>'
-
-    status = '200 OK'
-    response_headers = [('Content-type', 'text/html;charset=UTF-8'),
-                        ('Content-Length', str(len(output)))]
-    start_response(status, response_headers)
-
-    return [output]
