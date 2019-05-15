54103217fcb6f9869bf9980f20cf8babe9eef843
Jesús de Serdio <shamandul@gmail.com>
2019-05-15 21:38:02 +0200

    correcion del index para produccion

--- index.py
+++ index.py
@@ -4,7 +4,7 @@
 # Add the project to the python path
 import os, sys
 
-sys.path.append('/home/webweave/ refugio.webweaver.es/refugio/')
+sys.path.append('/home/webweave/refugio.webweaver.es/refugio/')
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
 
 # Prevent mod_wsgi from erroring on stdout access (WSGIRestrictStdout Off)
